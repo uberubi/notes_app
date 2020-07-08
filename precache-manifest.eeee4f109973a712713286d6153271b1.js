7a491f87a7fb8962dfaeb4785999aaa0d5223e6c
Andrew A. Stenipanin <and5251@yandex.ru>
2020-07-09 00:25:06 +0300

    Updates

---
+++ precache-manifest.eeee4f109973a712713286d6153271b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc48f265ca13336144e4420d6bebb58",
+    "url": "/notes_app/index.html"
+  },
+  {
+    "revision": "957a3b6e1a58bcf3f8f5",
+    "url": "/notes_app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7fe208b63a25ab252b35",
+    "url": "/notes_app/static/css/main.88ecfc41.chunk.css"
+  },
+  {
+    "revision": "957a3b6e1a58bcf3f8f5",
+    "url": "/notes_app/static/js/2.3a90a159.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/notes_app/static/js/2.3a90a159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe208b63a25ab252b35",
+    "url": "/notes_app/static/js/main.adb33c0b.chunk.js"
+  },
+  {
+    "revision": "c5847c3c4a6ef3324da3",
+    "url": "/notes_app/static/js/runtime-main.29dffd29.js"
+  },
+  {
+    "revision": "bf7ecd46a2a52b23f70ee75c328dbdb3",
+    "url": "/notes_app/static/media/register.bf7ecd46.svg"
+  }
+]);
